cc4f2f9625b3ae270a4ece70c33f5547bde3fca9
hasifahmed <hahmed@stuy.edu>
2018-11-06 20:23:36 -0500

    broken

--- dirinfo.c
+++ dirinfo.c
@@ -5,19 +5,23 @@
 #include <string.h>
 #include <errno.h>
 
-void dir(char * path){
-  DIR * stream = opendir(path);
-  if (errno != 0){
-    printf("Error: %s\n", strerror(errno));
+extern int errno;
 
-  } else {
+void dir(char * path){
+  DIR * stream;
   struct dirent * entry;
-  entry = readdir(stream);
+  struct stat * info = calloc(sizeof(struct stat),1);
+  long int totSize = 0;
+  char permissions[8][4] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
+
+
+
   //struct stat * info = malloc(sizeof(struct stat));
   //stat("Gondal.Adil.txt",info);
+
   printf("PERMISSIONS---------FILESIZE-------FILENAME\n");
-  char permissions[8][4] = {"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
-  long int totSize = 0;
+
+
 
   /*
   struct stat * info = malloc(sizeof(struct stat));
@@ -25,14 +29,19 @@ void dir(char * path){
   printf("%o\n",info->st_mode);
   */
 
+  stream = opendir(path);
+  if (errno != 0){
+    printf("Error: %s\n", strerror(errno));
+  }
+  entry = readdir(stream);
   while(entry){
-    struct stat * info = malloc(sizeof(struct stat));
+
     stat(entry -> d_name,info);
     //printf("%o        ",info->st_mode);
     int u = ((info->st_mode)/64)%8;
     int g = ((info->st_mode)/8)%8;
     int o = ((info->st_mode))%8;
-    char user[4];
+    //char user[4];
     if(S_ISDIR(info->st_mode)){
       printf("d");
     }
@@ -42,34 +51,33 @@ void dir(char * path){
     printf("%s",permissions[u]);
     printf("%s",permissions[g]);
     printf("%-13s",permissions[o]);
-    long int size = info ->st_size;
-    printf("%-15ld",size);
+
+    printf("%-15ld",info -> st_size);
     printf("%s\n",entry -> d_name);
 
-    totSize+= size;
-    free(info);
+    totSize+= info -> st_size;
+
     entry = readdir(stream);
     //dir(readdir(stream));
   }
-
+  free(info);
   closedir(stream);
 
   printf("Total Diectory Size: %ld Bytes\n",totSize);
-  }
+
 }
 int main(int argc, char *argv[]){
-  char dirname[100];
+
   if(argc > 1){
-    strcpy(dirname,argv[1]);
+    dir(argv[1]);
   }
   else{
-    char temp[100];
+    char dirname[100];
     printf("Type in valid path:\n");
-    scanf("%s", temp);
-    printf("%s\n",temp);
-    strcpy(dirname,temp);
+    scanf("%s", dirname);
+    dir(dirname);
   }
 
-  dir(dirname);
+
   return 0;
 }
